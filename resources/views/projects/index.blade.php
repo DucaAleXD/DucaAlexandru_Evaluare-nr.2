76733494f496add8be6f7e007799c7057f0657a2
DucaAleXD <ducasasha@gmail.com>
2024-03-26 10:26:48 +0200

    Corectarea greseli

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,14 +2,13 @@
 @section('content')
     <div class="container">
         <div class="row">
- 
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">
-                        <h2>Laravel 10 Crud</h2>
+                        <h2>Laravel 10 Crud Evaluare nr 2</h2>
                     </div>
                     <div class="card-body">
-                        <a href="{{ url('/project/create') }}" class="btn btn-success btn-sm" title="Add New Student">
+                        <a href="{{ url('/project/create') }}" class="btn btn-success btn-sm" title="Add New Project">
                             <i class="fa fa-plus" aria-hidden="true"></i> Add New
                         </a>
                         <br/>
@@ -19,36 +18,35 @@
                                 <thead>
                                     <tr>
                                         <th>#</th>
-                                        <th>Name</th>
-                                        <th>Address</th>
-                                        <th>Mobile</th>
-                                        <th>Actions</th>
+                                        <th>Nume</th>
+                                        <th>Descrierea</th>
+                                        <th>Starea</th>
+                                        <th>Data</th>
+                                        <th>Acțiuni</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($projects as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->name }}</td>
-                                        <td>{{ $item->address }}</td>
-                                        <td>{{ $item->mobile }}</td>
- 
-                                        <td>
-                                            <a href="{{ url('/project/' . $item->id) }}" title="View Project"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                            <a href="{{ url('/project/' . $item->id . '/edit') }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
- 
-                                            <form method="POST" action="{{ url('/project' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Project" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                    @foreach($projects as $project)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $project->name }}</td>
+                                            <td>{{ $project->description }}</td>
+                                            <td>{{ $project->status }}</td>
+                                            <td>{{ $project->date }}</td>
+                                            <td>
+                                                <a href="{{ url('/project/' . $project->id) }}" title="View Project"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                                                <a href="{{ url('/project/' . $project->id . '/edit') }}" title="Edit Project"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                                                <form method="POST" action="{{ url('/project/' . $project->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete Project" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
- 
                     </div>
                 </div>
             </div>
